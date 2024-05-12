d4ec46c124d102c341641dcdd6a7e59ee506f64c
bonbonsurce <aleksei.cherehovich@mail.ru>
2024-05-12 20:08:59 +0300

    rewrite generation data for highcharts

--- src/API/ChartsService.js
+++ src/API/ChartsService.js
@@ -10,6 +10,8 @@ export default class ChartsService {
         ];
 
         const chartsTitles = ['Chart 1', 'Chart 2', 'Chart 3'];
+        const chartTypes = ['line','column', 'bar', 'area', 'pie','spline'];
+        const yTitles = ['Values', 'Params'];
 
         for (let i = 0; i < numCharts; i++) {
             const xAxisLength = Math.floor(Math.random() * 6) + 5;
@@ -19,22 +21,30 @@ export default class ChartsService {
 
             const chartDate = datesWithTime[Math.floor(Math.random() * datesWithTime.length)];
             const series = [];
-            const numLines = Math.floor(Math.random() * 3) + 1;
+            let numLines = Math.floor(Math.random() * 3) + 1;
             const colors = ['#FFB6C1', '#87CEFA', '#ffe975', '#98FB98', '#FFA07A'];
+            let chartTitle = chartsTitles[Math.floor(Math.random() * chartsTitles.length)];
+            let chartType = chartTypes[Math.floor(Math.random() * chartTypes.length)];
+            let yTitle = yTitles[Math.floor(Math.random() * yTitles.length)];
+
+            let strDate = chartDate.toISOString();
+            let id = i + 1;
+            let countData = Math.floor(Math.random() * 8) + 3;
+            if (chartType === 'pie') {numLines = 1;}
 
             for (let j = 0; j < numLines; j++) {
                 const lineData = [];
-                for (let k = 0; k < xAxisLength; k++) {
-                    lineData.push((Math.random() * 10).toFixed(2));
+
+                for (let k = 0; k < countData; k++) {
+                    lineData.push(parseFloat((Math.random() * 10).toFixed(2)));
                 }
                 const randomIndex = Math.floor(Math.random() * colors.length);
-                series.push({ data: lineData, color: colors[randomIndex], area: true });
+                const seriesName = `Series ${j + 1}`;
+                series.push({ name: seriesName, data: lineData, color: colors[randomIndex]});
             }
 
-            let chartTitle = chartsTitles[Math.floor(Math.random() * chartsTitles.length)];
-            let strDate = chartDate.toISOString();
-            let id = i + 1;
-            charts.push({id, xAxis, series, strDate, chartTitle});
+
+            charts.push({id, xAxis, series, strDate, chartTitle, chartType, yTitle});
         }
         return charts;
     }
--- src/components/UI/ChartItem.jsx
+++ src/components/UI/ChartItem.jsx
@@ -6,6 +6,8 @@ import MySelect from "./select/MySelect";
 import MyButton from "./button/MyButton";
 import MyModal from "./MyModal/MyModal";
 import ChartForm from "../ChartForm";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 const ChartItem = (props) => {
     const {isSettings, setIsSettings, setIsLoading} = useContext(SettingsContext);
@@ -33,30 +35,77 @@ const ChartItem = (props) => {
         hideModals();
     };
 
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    console.log(props.chart.series)
+    const options = {
+        chart: {type: props.chart.chartType},
+        title: {text: props.chart.chartTitle},
+        yAsix: {title: {text: props.chart.yTitle}},
+        series: props.chart.series,
+        xAxis: {categories: months}
+    }
+    // chart: {
+    //     type: 'spline'
+    // },
+    // title: {
+    //     text: 'My chart'
+    // },
+    // xAxis: {
+    //     categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'] // Значения для оси X
+    // },
+    // yAxis: {
+    //     title: {
+    //         text: 'Values'
+    //     }
+    // },
+    // series: [
+    //     {
+    //         name: 'Series 1', // Название серии данных
+    //         color: 'blue', // Цвет линии
+    //         data: [1, 2, 1, 4, 3, 6] // Данные для серии
+    //     }
+    // ]
+    const chartTypes = ['line','column', 'bar', 'area', 'pie','spline'];
+
     return (
         <div className="chart-content">
-            <h2 style={{padding:10, textAlign:'center'}}>{props.chart.chartTitle}</h2>
-            <h3 style={{padding:20}}> Date: {strDate}</h3>
-            <LineChart
-                xAxis={props.chart.xAxis}
-                series={props.chart.series}
-                width={600}
-                height={300}
-            />
+            {/*<h2 style={{padding:10, textAlign:'center'}}>{props.chart.chartTitle}</h2>*/}
+
+            {/*<LineChart*/}
+            {/*    xAxis={props.chart.xAxis}*/}
+            {/*    series={props.chart.series}*/}
+            {/*    width={600}*/}
+            {/*    height={300}*/}
+            {/*/>*/}
+
+            <HighchartsReact highcharts={Highcharts} options={options}/>
+            <h3 style={{padding: 20}}> Date: {strDate}</h3>
+
             {isSettings &&
                 <div className="navbar">
                     <div>
                         <MyButton onClick={showEditModal}>Edit</MyButton>
                         <MyModal visible={editModalVisible} setVisible={setEditModalVisible}>
-                            <ChartForm edit={props.edit} chartsCount={charts.length} editChart={props.chart} hideModal={hideModals}/>
+                            <ChartForm edit={props.edit} chartsCount={charts.length} editChart={props.chart}
+                                       hideModal={hideModals}/>
                         </MyModal>
                     </div>
 
                     <div>
                         <MyButton onClick={showDeleteModal}>Delete</MyButton>
                         <MyModal visible={deleteModalVisible} setVisible={setDeleteModalVisible}>
-                            <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', padding:10}}>
-                                <h5 style={{marginBottom:25}}>Are you sure that you want to delete this chart?</h5>
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                padding: 10
+                            }}>
+                                <h5 style={{marginBottom: 25}}>Are you sure that you want to delete this chart?</h5>
                                 <MyButton onClick={deleteChart}>Delete</MyButton>
                             </div>
                         </MyModal>
